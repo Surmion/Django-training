c1909f009435e1f030c4c3c3ea39e7372c4f5d30
unknown <jamesbryansurmion1@gmail.com>
2025-08-26 23:08:51 +0800

    new model product

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path 
-from .views import ItemList, ItemDetail, LocationList, LocationDetail
+from .views import *
 
 urlpatterns = [
     path('item/', ItemList.as_view()),
     path('item/<int:pk>/', ItemDetail.as_view()),
     path('location/', LocationList.as_view()),
     path('location/<int:pk>/', LocationDetail.as_view()),
+    path('product/', ProductList.as_view()),
+    path('product/<int:pk>/', ProductDetail.as_view()),
+
 ]
